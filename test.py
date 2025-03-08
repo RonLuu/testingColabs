c2e7d99fc48cb6788bd0483d62c0affc39e6e4c5
troppydash <troppydash.developer@gmail.com>
2025-03-09 00:13:52 +1100

    Adding multiplication

--- test.py
+++ test.py
@@ -2,6 +2,12 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a - b 
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+
+
 print("hello world")
 print(add(4,5))
